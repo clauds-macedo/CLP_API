68c657e583948736e1ff9a11ff33233c04c4ccee
clauds-macedo <yanalogic@gmail.com>
2022-02-02 19:30:04 -0300

    fixed -> parsed post cpf to int

--- methods/postNewCPF.jl
+++ methods/postNewCPF.jl
@@ -1,13 +1,13 @@
 function postCPF(cpf, name)
     
     newCPF = Dict{String, Any}(
-        "id" => "$cpf",
+        "id" => parse(Int64 ,"$cpf"),
         "name" => "$name",
         "pagamentos" => []
     )
     
     newCPF = JSON.json(newCPF)
     
-    return HTTP.post("https://tarry-malachite-divan.glitch.me/cpf/", [("Content-Type", "application/json")], newCPF)
+   return HTTP.post("https://tarry-malachite-divan.glitch.me/cpf/", [("Content-Type", "application/json")], newCPF)
     
 end
